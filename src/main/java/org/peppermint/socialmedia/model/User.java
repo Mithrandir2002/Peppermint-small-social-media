32603ce96128cdb134d22766c1eb88881af94591
Nguyen Hoang Phuong Nam <namnhp.22bi13323@usth.edu.vn>
2024-12-19 00:14:05 +0700

    add id attribute to User model

--- src/main/java/org/peppermint/socialmedia/model/User.java
+++ src/main/java/org/peppermint/socialmedia/model/User.java
@@ -1,2 +1,18 @@
-package org.peppermint.socialmedia.model;public class User {
+package org.peppermint.socialmedia.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
 }
