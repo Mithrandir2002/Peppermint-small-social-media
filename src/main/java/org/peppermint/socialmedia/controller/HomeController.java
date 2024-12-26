ed6506f24020faa051845d5a49ac9387317c9801
Nguyen Hoang Phuong Nam <namnhp.22bi13323@usth.edu.vn>
2024-12-26 23:21:46 +0700

    :/

--- src/main/java/org/peppermint/socialmedia/controller/HomeController.java
+++ src/main/java/org/peppermint/socialmedia/controller/HomeController.java
@@ -1,7 +1,13 @@
 package org.peppermint.socialmedia.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
+
+    @GetMapping
+    public String homeControllerHandler() {
+        return "this is home controller";
+    }
 }
