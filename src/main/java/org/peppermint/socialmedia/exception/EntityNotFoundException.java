44da4a8990e3f5bb16c24f71bf1c9dc871b5e406
Nguyen Hoang Phuong Nam <namnhp.22bi13323@usth.edu.vn>
2024-12-26 23:19:59 +0700

    add login request class to represent username-password login method

--- src/main/java/org/peppermint/socialmedia/exception/EntityNotFoundException.java
+++ src/main/java/org/peppermint/socialmedia/exception/EntityNotFoundException.java
@@ -1,2 +1,9 @@
-package org.peppermint.socialmedia.exception;public class EntityNotFoundException {
+package org.peppermint.socialmedia.exception;
+
+import java.util.Locale;
+
+public class EntityNotFoundException extends RuntimeException{
+    public EntityNotFoundException(Integer id, Class<?> entity) {
+        super("The " + entity.getSimpleName().toLowerCase() + " with id " + id + " does not exist in our records");
+    }
 }
--- src/main/java/org/peppermint/socialmedia/exception/UserExistedException.java
+++ src/main/java/org/peppermint/socialmedia/exception/UserExistedException.java
@@ -1,2 +1,7 @@
-package org.peppermint.socialmedia.exception;public class UserExistedException {
+package org.peppermint.socialmedia.exception;
+
+public class UserExistedException extends RuntimeException{
+    public UserExistedException(String email) {
+        super("The email " + email + " is already registered in our records");
+    }
 }
