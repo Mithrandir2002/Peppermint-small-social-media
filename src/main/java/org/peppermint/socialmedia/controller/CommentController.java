4c97c6e9e1d97897fb300a1f45eb063d18c6cfa8
Nguyen Hoang Phuong Nam <namnhp.22bi13323@usth.edu.vn>
2025-02-10 16:16:53 +0700

    add get comment by id to comment controller

--- src/main/java/org/peppermint/socialmedia/controller/CommentController.java
+++ src/main/java/org/peppermint/socialmedia/controller/CommentController.java
@@ -6,6 +6,11 @@ import org.peppermint.socialmedia.model.User;
 import org.peppermint.socialmedia.service.CommentService;
 import org.peppermint.socialmedia.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -34,5 +39,17 @@ public class CommentController {
     public Comment updateComment(@RequestHeader("Authorization") String jwt, @RequestBody Comment comment, @PathVariable Integer commentId) {
         User user = userService.findUserByJwt(jwt);
         return commentService.updateComment(comment, commentId, user.getId());
+    }
+
+    @GetMapping("api/comments/{postId}")
+    public ResponseEntity<Page<Comment>> getCommentsByPostId(@RequestHeader("Authorization") String jwt,
+                                              @PathVariable("postId") Integer postId,
+                                              @RequestParam(value = "offset", required = false) Integer offset,
+                                              @RequestParam(value = "pageSize", required = false) Integer pageSize,
+                                              @RequestParam(value = "sortBy", required = false) String sortBy) {
+        if (offset == null) offset = 0;
+        if (pageSize == null) pageSize = 20;
+        if (sortBy.equals("")) sortBy = "id";
+        return new ResponseEntity<>(commentService.pageableCommentbyPostId(postId, PageRequest.of(offset, pageSize, Sort.by(sortBy))), HttpStatus.OK);
     }
 }
