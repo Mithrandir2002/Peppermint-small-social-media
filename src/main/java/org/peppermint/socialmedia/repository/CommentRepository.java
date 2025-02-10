788e48888e9163139affadaa0730f98a48428b3a
Nguyen Hoang Phuong Nam <namnhp.22bi13323@usth.edu.vn>
2025-02-10 16:15:57 +0700

    jpql comment repository to get pagnition comment

--- src/main/java/org/peppermint/socialmedia/repository/CommentRepository.java
+++ src/main/java/org/peppermint/socialmedia/repository/CommentRepository.java
@@ -1,7 +1,14 @@
 package org.peppermint.socialmedia.repository;
 
 import org.peppermint.socialmedia.model.Comment;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface CommentRepository extends JpaRepository<Comment, Integer> {
+
+    @Query("select c from Comment c where c.post.id = :postId")
+    Page<Comment> getCommentFromPostId(@Param("postId") Integer postId, Pageable of);
 }
